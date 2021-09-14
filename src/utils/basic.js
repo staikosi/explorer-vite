66d761c5fbd8e9fd5c9c621a1b0e6bc00e531053
staikosi <staikosi@anonymous>
2021-09-14 05:18:29 +0000

    fix: merge arr

--- src/utils/basic.js
+++ src/utils/basic.js
@@ -1,4 +1,7 @@
 export function mergeArr(arr1, arr2, keyFn) {
+  if (!arr2) return arr1;
+  if (!arr1) return arr2;
+  if (!arr1 && !arr2) return undefined;
   let seen = {};
   const result = arr1.concat(arr2).filter((item, pos) => {
     // distinct
